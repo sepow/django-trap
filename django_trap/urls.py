7cec7cb99f98f21548a35227b53798cba8ab117f
Sean Powell <bendtsenpowell@gmail.com>
2015-11-04 19:45:22 +0100

    urls.patterns deprecated

--- django_trap/urls.py
+++ django_trap/urls.py
@@ -8,15 +8,13 @@ try:
 except ImportError:  # django < 1.4
     from django.conf.urls.defaults import patterns, url
 
-
-urlpatterns = patterns('')
-
 # Add /admin/login/ as a separate named view in Django 1.7+
 if django.VERSION >= (1, 7):
-    urlpatterns += patterns('',
+    urlpatterns = [
         url(r'^login/$', views.AdminHoneypot.as_view(), name='login'),
+        url(r'^.*$', views.AdminHoneypot.as_view(), name='index'),
+    ]
+else:
+    urlpatterns = patterns('',
+        url(r'^.*$', views.AdminHoneypot.as_view(), name='index'),
     )
-
-urlpatterns += patterns('',
-    url(r'^.*$', views.AdminHoneypot.as_view(), name='index'),
-)
